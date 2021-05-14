db504202d120be51d18ec53240bf1dea260249c8
rmctJ <11417719+rmctJ@users.noreply.github.com>
2021-05-14 22:53:06 +0900

    Update main for gae

--- main.go
+++ main.go
@@ -3,8 +3,32 @@ package main
 import (
 	calc "action_demo_go/calculator"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/pi" {
+		fmt.Fprint(w, calc.Pi())
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
 func main() {
-	fmt.Println(calc.Pi())
+	http.HandleFunc("/", indexHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Default port %s", port)
+	} else {
+		log.Printf("Listening port %s", port)
+	}
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	
 }
